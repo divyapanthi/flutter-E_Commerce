e2f8363e0079da77539e48cc29b4b0a481593b84
Divya <deevsofficial341@gmail.com>
2021-10-12 16:53:01 +0545

    Add pop menu item for showing favourite items and all items

--- lib/screens/product_overview_screen.dart
+++ lib/screens/product_overview_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_e_commerce/widgets/product_grid.dart';
 
+enum FilterOptions { Favourites, All}
+
 class ProductOverviewScreen extends StatefulWidget {
   const ProductOverviewScreen({Key? key}) : super(key: key);
 
@@ -9,16 +11,39 @@ class ProductOverviewScreen extends StatefulWidget {
 }
 
 class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
+  bool _showFavourites = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Shop Venue",
-          style: TextStyle(fontFamily: "Raleway"),
+        appBar: AppBar(
+          title: const Text(
+            "Shop Venue",
+            style: TextStyle(fontFamily: "Raleway"),
+          ),
+          actions: <Widget>[
+            PopupMenuButton(
+              icon: const Icon(Icons.more_vert),
+              onSelected: (FilterOptions selectedValue) {
+                setState(() {
+                  if (selectedValue == FilterOptions.Favourites) {
+                    //show fav
+                    _showFavourites = true;
+                  } else {
+                    _showFavourites = false;
+                  }
+                });
+              },
+              itemBuilder: (context) => [
+                const PopupMenuItem(
+                    value: FilterOptions.Favourites,
+                    child: Text('Show Favourites')),
+                const PopupMenuItem(
+                    value: FilterOptions.All,
+                    child: Text("Show All")),
+              ],
+            )
+          ],
         ),
-      ),
-      body: const ProductGrid()
-    );
+        body: ProductGrid(_showFavourites));
   }
 }
--- lib/widgets/product_grid.dart
+++ lib/widgets/product_grid.dart
@@ -1,16 +1,19 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_e_commerce/models/products.dart';
 import 'package:flutter_e_commerce/widgets/product_item.dart';
 import 'package:provider/provider.dart';
 
 class ProductGrid extends StatelessWidget {
+  final bool? isFavourite;
 
-  const ProductGrid({Key? key}) : super(key: key);
+  const ProductGrid(this.isFavourite, {Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
-    final products = Provider.of<Products>(context).items;
+    final products = isFavourite!
+        ? Provider.of<Products>(context).favourites
+        : Provider.of<Products>(context).items;
     return GridView.builder(
       itemCount: products.length,
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -21,7 +24,7 @@ class ProductGrid extends StatelessWidget {
       ),
       itemBuilder: (ctx, index) => ChangeNotifierProvider.value(
         value: products[index],
-        child:  ProductItem(),
+        child: const ProductItem(),
       ),
     );
   }
